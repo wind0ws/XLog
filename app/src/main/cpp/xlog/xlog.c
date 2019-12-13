aeb655bb425aa4d8bda9409a06816a0d4d579b6c
Threshold <932803995@qq.com>
2019-12-13 16:59:34 +0800

    fix MSVC(Visual Studio) compiler don't remove redundant comma

--- app/src/main/cpp/xlog/xlog.c
+++ app/src/main/cpp/xlog/xlog.c
@@ -1,8 +1,14 @@
-//
-// Created by Administrator on 2019/11/5.
-//
-
 #include "xlog.h"
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif// __cplusplus
 
 int LOG_CONFIG_LEVEL = LOG_LEVEL_VERBOSE;
 int LOG_CONFIG_TARGET = (LOG_TARGET_ANDROID | LOG_TARGET_CONSOLE); // NOLINT(hicpp-signed-bitwise)
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
