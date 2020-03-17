b369c4d0a3e1bf3221ef28c3c96a5ee1b47e70c3
Threshold <932803995@qq.com>
2020-03-17 17:43:39 +0800

    fix wrong log level when log to console

--- app/src/main/cpp/main.c
+++ app/src/main/cpp/main.c
@@ -2,7 +2,6 @@
 #define LOG_TAG_MAIN "MY_TAG"
 
 int main(int argc, char *argv[]){
-
     LOGD("\n%s\nHello! current log_priority=%d, log_target=%d\n%s",
             LOG_LINE_STAR, xlog_config_level, xlog_config_target, LOG_LINE_STAR)
 
@@ -18,29 +17,32 @@ int main(int argc, char *argv[]){
     LOGW_TRACE("this log is printed by LOGW_TRACE")
     LOGE_TRACE("this log is printed by LOGE_TRACE")
 
-    TLOGV(LOG_TAG_MAIN,"this log is printed by TLOGV")
-    TLOGD(LOG_TAG_MAIN,"this log is printed by TLOGD")
-    TLOGI(LOG_TAG_MAIN,"this log is printed by TLOGI")
-    TLOGW(LOG_TAG_MAIN,"this log is printed by TLOGW")
-    TLOGE(LOG_TAG_MAIN,"this log is printed by TLOGE")
+    TLOGV(LOG_TAG_MAIN, "this log is printed by TLOGV")
+    TLOGD(LOG_TAG_MAIN, "this log is printed by TLOGD")
+    TLOGI(LOG_TAG_MAIN, "this log is printed by TLOGI")
+    TLOGW(LOG_TAG_MAIN, "this log is printed by TLOGW")
+    TLOGE(LOG_TAG_MAIN, "this log is printed by TLOGE")
 
-    TLOGV_TRACE(LOG_TAG_MAIN,"this log is printed by TLOGV_TRACE")
-    TLOGD_TRACE(LOG_TAG_MAIN,"this log is printed by TLOGD_TRACE")
-    TLOGI_TRACE(LOG_TAG_MAIN,"this log is printed by TLOGI_TRACE")
-    TLOGW_TRACE(LOG_TAG_MAIN,"this log is printed by TLOGW_TRACE")
-    TLOGE_TRACE(LOG_TAG_MAIN,"this log is printed by TLOGE_TRACE")
+    TLOGV_TRACE(LOG_TAG_MAIN, "this log is printed by TLOGV_TRACE")
+    TLOGD_TRACE(LOG_TAG_MAIN, "this log is printed by TLOGD_TRACE")
+    TLOGI_TRACE(LOG_TAG_MAIN, "this log is printed by TLOGI_TRACE")
+    TLOGW_TRACE(LOG_TAG_MAIN, "this log is printed by TLOGW_TRACE")
+    TLOGE_TRACE(LOG_TAG_MAIN, "this log is printed by TLOGE_TRACE")
 
     xlog_config_level = LOG_LEVEL_ERROR;
     xlog_config_target = LOG_TARGET_ANDROID;
     LOGI("this log won't print because of current level is LOG_LEVEL_ERROR")
     LOGE("this log only print on logcat because of xlog_config_target=LOG_TARGET_ANDROID")
 
+    xlog_config_target = LOG_TARGET_CONSOLE;
+    LOGE_TRACE("this log only print on console because of xlog_config_target=LOG_TARGET_CONSOLE")
+
     xlog_config_level = LOG_LEVEL_OFF;
     LOGE("this log won't print because of xlog_config_level = LOG_LEVEL_OFF")
 
     xlog_config_level = LOG_LEVEL_INFO;
-    xlog_config_target = LOG_TARGET_ANDROID | LOG_TARGET_CONSOLE;
-    LOGI("TEST finished!!!")
+    xlog_config_target = (LOG_TARGET_ANDROID | LOG_TARGET_CONSOLE);
+    LOGI("LOG test finished!!!")
 
     return 0;
 }
--- app/src/main/cpp/xlog/xlog.c
+++ app/src/main/cpp/xlog/xlog.c
@@ -5,12 +5,12 @@ extern "C"
 {
 #endif// __cplusplus
 
-	int xlog_config_level = LOG_LEVEL_VERBOSE;
+int xlog_config_level = LOG_LEVEL_VERBOSE;
 #ifdef _MSC_VER
-	int xlog_config_target = LOG_TARGET_CONSOLE;
+int xlog_config_target = LOG_TARGET_CONSOLE;
 #else
-	int xlog_config_target = (LOG_TARGET_ANDROID | LOG_TARGET_CONSOLE); // NOLINT(hicpp-signed-bitwise)
-#endif // _MSC_VER	
+int xlog_config_target = (LOG_TARGET_ANDROID | LOG_TARGET_CONSOLE); // NOLINT(hicpp-signed-bitwise)
+#endif // _MSC_VER
 
 #ifdef __cplusplus
 };
